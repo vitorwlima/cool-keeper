ab718e082deb9a6fb017ba0628a25a675f787c0e
Vitor Lima <vitorwlima13@gmail.com>
2022-06-15 11:42:45 -0300

    add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
+import 'src/styles/global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
