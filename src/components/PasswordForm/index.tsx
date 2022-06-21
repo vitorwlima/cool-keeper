d89e154cfa60e7a7a12f2422dec060d90a8ed716
Vitor Lima <vitorwlima13@gmail.com>
2022-06-20 23:47:56 -0300

    feat: add edit/update of password with responsive UI (super cool)

--- src/components/PasswordForm/index.tsx
+++ src/components/PasswordForm/index.tsx
@@ -1,17 +1,28 @@
 import { FormHandles, SubmitHandler } from '@unform/core'
 import { Form } from '@unform/web'
 import { FC, Ref } from 'react'
+import { inferQueryResponse } from 'src/pages/api/trpc/[trpc]'
 import { Input } from '../Input'
 
 type Props = {
   onSubmit: SubmitHandler<any>
   formRef: Ref<FormHandles>
   isLoading: boolean
+  currentPassword?: inferQueryResponse<'passwords.get'>[number]
+  onDeletePassword?: () => void
 }
 
-export const PasswordForm: FC<Props> = ({ onSubmit, formRef, isLoading }) => {
+export const PasswordForm: FC<Props> = ({ onSubmit, formRef, isLoading, currentPassword, onDeletePassword }) => {
+  const initialData = currentPassword
+    ? {
+      name: currentPassword.name,
+      login: currentPassword.login,
+      password: currentPassword.decrypted_password
+    }
+    : {}
+
   return (
-    <Form className="flex flex-col gap-3" onSubmit={onSubmit} ref={formRef}>
+    <Form className="flex flex-col gap-3" onSubmit={onSubmit} ref={formRef} initialData={initialData}>
       <div className="form-control w-full">
         <Input
           name="name"
@@ -43,6 +54,11 @@ export const PasswordForm: FC<Props> = ({ onSubmit, formRef, isLoading }) => {
       >
         Save
       </button>
+      {!!currentPassword && (
+        <button className="btn btn-error" onClick={() => onDeletePassword!()}>
+          Delete password
+        </button>
+      )}
     </Form>
   )
 }
--- src/components/PasswordsList/index.tsx
+++ src/components/PasswordsList/index.tsx
@@ -1,60 +1,125 @@
-import { FC } from 'react'
-import { FiCopy, FiTrash } from 'react-icons/fi'
+import { FormHandles, SubmitHandler } from '@unform/core'
+import { useRouter } from 'next/router'
+import { FC, MouseEvent, useRef, useState } from 'react'
+import { FiCopy } from 'react-icons/fi'
 import { inferQueryResponse } from 'src/pages/api/trpc/[trpc]'
 import { notify } from 'src/utils/notify'
 import { trpc } from 'src/utils/trpc'
+import { PasswordForm } from '../PasswordForm'
 
 type Props = {
   passwords: inferQueryResponse<'passwords.get'>
 }
 
+type FormData = {
+  login: string
+  name: string
+  password: string
+}
+
+type Password = inferQueryResponse<'passwords.get'>[number]
+
 export const PasswordsList: FC<Props> = ({ passwords }) => {
+  const router = useRouter()
+  const trpcContext = trpc.useContext()
+  const formRef = useRef<FormHandles>(null)
+  const [editingPassword, setEditingPassword] = useState<Password | undefined>(undefined)
+
   if (passwords.length === 0) {
     return <p className="text-center text-xl">No saved passwords :(</p>
   }
 
-  const trpcContext = trpc.useContext()
-  const { mutate } = trpc.useMutation('passwords.delete', {
+  const { mutate: deleteMutate } = trpc.useMutation('passwords.delete', {
     onSuccess: () => {
+      setEditingPassword(undefined)
       notify('success', 'Password deleted successfully.')
       trpcContext.invalidateQueries('passwords.get')
     }
   })
 
-  const handleCopyPassword = (password: string) => {
+  const { mutate: updateMutate } = trpc.useMutation('passwords.update', {
+    onSuccess: (data) => {
+      setEditingPassword(data)
+      notify('success', 'Password updated successfully.')
+      trpcContext.invalidateQueries('passwords.get')
+    }
+  })
+
+  const handleSetEditingPassword = (password: Password) => {
+    if (Object.is(password, editingPassword)) {
+      setEditingPassword(undefined)
+      return
+    }
+
+    setEditingPassword(password)
+  }
+
+  const handleDeletePassword = () => {
+    deleteMutate({ passwordId: editingPassword!.id })
+  }
+
+  const handleCopyPassword = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>, password: string) => {
+    event.stopPropagation()
     navigator.clipboard.writeText(password)
   }
 
-  const handleDeletePassword = (passwordId: string) => {
-    mutate({ passwordId })
+  const handleSubmit: SubmitHandler<FormData> = async (data) => {
+    updateMutate({ ...data, id: editingPassword!.id })
+  }
+
+  const handleRedirectSaveNewPassword = () => {
+    router.push('/save')
   }
 
   return (
-    <ul className="flex flex-col gap-4">
-      {passwords.map((password) => (
-        <li className="card shadow-xl" key={password.id}>
-          <div className="flex p-4 justify-between w-full">
-            <div>
-              <h4 className="card-title">{password.name}</h4>
-              <p>{password.login}</p>
-            </div>
-            <div className="flex gap-2">
-              <button
-                className="btn btn-primary"
-                onClick={() => handleCopyPassword(password.decrypted_password)}
-              >
-                <FiCopy />
-              </button>
-              <button
-                className="btn"
-                onClick={() => handleDeletePassword(password.id)}
-              >
-                <FiTrash />
-              </button>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="flex gap-20">
+      <section className={`flex-1 lg:block ${editingPassword ? 'hidden' : ''}`}>
+        <header className="flex items-center justify-between mb-8">
+          <h2 className="font-semibold text-lg">Passwords</h2>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleRedirectSaveNewPassword()}
+          >
+            Save new
+          </button>
+        </header>
+        <ul className="flex flex-col gap-4 overflow-y-scroll max-h-[calc(100vh_-_306px)] pr-4 scrollbar-thumb-zinc-800 scrollbar-thin">
+          {passwords.map((password) => (
+            <li className="shadow-xl cursor-pointer" key={password.id} onClick={() => handleSetEditingPassword(password)}>
+              <div className="flex p-4 justify-between w-full">
+                <div>
+                  <h4 className="card-title">{password.name}</h4>
+                  <p>{password.login}</p>
+                </div>
+                <div className="flex gap-2">
+                  <button
+                    className="btn btn-primary"
+                    onClick={(e) => handleCopyPassword(e, password.decrypted_password)}
+                  >
+                    <FiCopy />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+      {!!editingPassword &&
+        <section className='flex-1'>
+          <header className="mb-4 flex items-center justify-between">
+            <p className="text-2xl font-bold">{editingPassword.name}</p>
+            <button className="btn lg:hidden" onClick={() => setEditingPassword(undefined)}>Back</button>
+          </header>
+          <PasswordForm
+            formRef={formRef}
+            onSubmit={handleSubmit}
+            isLoading={false}
+            currentPassword={editingPassword}
+            key={editingPassword.id}
+            onDeletePassword={handleDeletePassword}
+          />
+        </section>
+      }
+    </div>
   )
 }
