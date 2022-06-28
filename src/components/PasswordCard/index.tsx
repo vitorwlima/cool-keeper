9f3d771cb87148dd58ab218a79273913240a52ad
Vitor Lima <vitorwlima13@gmail.com>
2022-06-28 18:58:38 -0300

    feat: new component for password card and better UI

---
+++ src/components/PasswordCard/index.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames'
+import { FC, MouseEvent } from 'react'
+import { FiCopy } from 'react-icons/fi'
+import { inferQueryResponse } from 'src/pages/api/trpc/[trpc]'
+import { notify } from 'src/utils/notify'
+
+type Props = {
+  password: inferQueryResponse<'passwords.get'>[number]
+  editPassword: (password: inferQueryResponse<'passwords.get'>[number]) => void
+  active: boolean
+}
+
+export const PasswordCard: FC<Props> = ({ password, editPassword, active }) => {
+  const handleCopyPassword = (
+    event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+    password: string,
+  ) => {
+    event.stopPropagation()
+    navigator.clipboard.writeText(password)
+    notify('success', 'Password copied to clipboard.')
+  }
+
+  return (
+    <li
+      className={classNames('shadow-xl cursor-pointer rounded-lg transition-colors', {
+        'bg-base-300 hover:bg-base-200': !active,
+        'bg-primary hover:bg-primary-focus text-primary-content': active,
+      })}
+      onClick={() => editPassword(password)}
+    >
+      <div className='flex p-4 justify-between w-full'>
+        <div>
+          <h4 className='card-title'>{password.name}</h4>
+          <p>{password.login}</p>
+        </div>
+        <div className='flex gap-2'>
+          <button
+            className='flex items-center justify-center p-1 text-neutral-content hover:text-primary-content'
+            onClick={(e) => handleCopyPassword(e, password.decrypted_password)}
+          >
+            <FiCopy />
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
--- src/components/PasswordsList/index.tsx
+++ src/components/PasswordsList/index.tsx
@@ -1,10 +1,10 @@
 import { FormHandles, SubmitHandler } from '@unform/core'
 import { useRouter } from 'next/router'
-import { FC, MouseEvent, useRef, useState } from 'react'
-import { FiCopy } from 'react-icons/fi'
+import { FC, useRef, useState } from 'react'
 import { inferQueryResponse } from 'src/pages/api/trpc/[trpc]'
 import { notify } from 'src/utils/notify'
 import { trpc } from 'src/utils/trpc'
+import { PasswordCard } from '../PasswordCard'
 import { PasswordForm } from '../PasswordForm'
 
 type Props = {
@@ -54,14 +54,6 @@ export const PasswordsList: FC<Props> = ({ passwords }) => {
     deleteMutate({ passwordId: editingPassword!.id })
   }
 
-  const handleCopyPassword = (
-    event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
-    password: string,
-  ) => {
-    event.stopPropagation()
-    navigator.clipboard.writeText(password)
-  }
-
   const handleSubmit: SubmitHandler<FormData> = async (data) => {
     updateMutate({ ...data, id: editingPassword!.id })
   }
@@ -82,26 +74,12 @@ export const PasswordsList: FC<Props> = ({ passwords }) => {
         <ul className='flex flex-col gap-4 overflow-y-scroll max-h-[calc(100vh_-_306px)] pr-4 scrollbar-thumb-zinc-800 scrollbar-thin'>
           {passwords.length ? (
             passwords.map((password) => (
-              <li
-                className='shadow-xl cursor-pointer'
+              <PasswordCard
+                password={password}
                 key={password.id}
-                onClick={() => handleSetEditingPassword(password)}
-              >
-                <div className='flex p-4 justify-between w-full'>
-                  <div>
-                    <h4 className='card-title'>{password.name}</h4>
-                    <p>{password.login}</p>
-                  </div>
-                  <div className='flex gap-2'>
-                    <button
-                      className='btn btn-primary'
-                      onClick={(e) => handleCopyPassword(e, password.decrypted_password)}
-                    >
-                      <FiCopy />
-                    </button>
-                  </div>
-                </div>
-              </li>
+                editPassword={handleSetEditingPassword}
+                active={password.id === editingPassword?.id}
+              />
             ))
           ) : (
             <p className='text-center text-xl'>No saved passwords :(</p>
